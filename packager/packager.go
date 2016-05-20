42a2a950b3b26441f204110de5b448d236a0cbde
Duane Bekaert <duane.bekaert@gmail.com>
2016-05-20 09:48:32 +0200

    Recursively package files in a byte array map

--- packager/packager.go
+++ packager/packager.go
@@ -2,8 +2,39 @@ package packager
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
-func Create(path string, output string) {
-	fmt.Println("GO NINJA")
+var Content map[string][]byte = make(map[string][]byte)
+
+func Create(path, output string) {
+	Traverse(path)
+	Write()
+}
+
+func Traverse(dirname string) {
+	content, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		fmt.Println("Error opening " + dirname + " ...")
+	}
+	for _, f := range content {
+		if !strings.HasPrefix(f.Name(), ".") {
+			if f.IsDir() {
+				Traverse(dirname + f.Name())
+			} else {
+				content, err := ioutil.ReadFile(dirname + f.Name())
+				if err != nil {
+					fmt.Println("Error while trying to open " + dirname + f.Name() + " (" + err.Error() + ").")
+				} else {
+					Content[dirname+f.Name()] = content
+					fmt.Println("File " + dirname + f.Name() + " successfully packaged.")
+				}
+			}
+		}
+	}
+}
+
+func Write() {
+	fmt.Println(Content)
 }
