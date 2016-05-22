d65688f2c85b2c4542305e093c0a981d44b4ad7f
Duane Bekaert <duane.bekaert@gmail.com>
2016-05-22 23:18:42 +0200

    Packager should be done; have to try it out

--- packager/packager.go
+++ packager/packager.go
@@ -1,18 +1,23 @@
 package packager
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"strings"
 )
 
+// The content that will be packaged
 var Content map[string][]byte = make(map[string][]byte)
 
 func Create(path, output string) {
+	//Test := []byte("Install gentoo")
 	Traverse(path)
-	Write()
+	Write(output)
 }
 
+// Traverse the content of dirname recursively and add every file in the
+// Content map
 func Traverse(dirname string) {
 	content, err := ioutil.ReadDir(dirname)
 	if err != nil {
@@ -25,16 +30,32 @@ func Traverse(dirname string) {
 			} else {
 				content, err := ioutil.ReadFile(dirname + f.Name())
 				if err != nil {
-					fmt.Println("Error while trying to open " + dirname + f.Name() + " (" + err.Error() + ").")
+					fmt.Println("Error while trying to open " +
+						dirname + f.Name() + " (" + err.Error() + ").")
 				} else {
 					Content[dirname+f.Name()] = content
-					fmt.Println("File " + dirname + f.Name() + " successfully packaged.")
+					fmt.Println("File " + dirname + f.Name() +
+						" successfully packaged.")
 				}
 			}
 		}
 	}
 }
 
-func Write() {
+// Write the Content map to a file
+func Write(path string) {
 	fmt.Println(Content)
+	filecontent := []byte(
+		"package main\n\nvar SpacContent = map[string][]byte = {\n")
+	for key, content := range Content {
+		_ = content
+		fmt.Println(key)
+		filecontent = append(filecontent,
+			[]byte("    \""+key+"\": []byte(\"")...)
+		filecontent = append(filecontent,
+			bytes.Replace(content, []byte("\""), []byte("\\\""), -1)...)
+		filecontent = append(filecontent, []byte("\"),")...)
+	}
+	filecontent = append(filecontent, []byte("]")...)
+	ioutil.WriteFile(path, filecontent, 0644)
 }
